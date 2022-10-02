0b92bcf315fa8e8775e480773b3605f8a98ad7e2
eballhysa <ballhysa@hotmail.com>
2022-10-02 22:27:52 +0200

    Initial commit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
 
   def show
-    id = params[:id] # retrieve movie ID from URI route
+    id = params['id'] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
